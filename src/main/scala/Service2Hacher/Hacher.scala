42109e255d0c02de5da09ff104296ba1a6c4ca37
yorgunmichael <yorgunchacha@yahoo.fr>
2022-12-05 12:30:32 +0100

    test

--- src/main/scala/Service2Hacher/Hacher.scala
+++ src/main/scala/Service2Hacher/Hacher.scala
@@ -2,36 +2,66 @@ package complianceGRPD
 package Service2Hacher
 
 import complianceGRPD.SchemaDonnee
-import org.apache.spark.sql.SparkSession
+import complianceGRPD.Service1Delete.schema
+import javafx.scene.chart.PieChart.Data
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
 
 import java.util.UUID
 
 object Hacher {
+
   import org.apache.spark.sql.functions._
 
   def hacher(): Unit = {
 
-    val sparkSession: SparkSession = SparkSession.builder().master("spark://172.31.250.9:7077").getOrCreate()
+    val sparkSession: SparkSession = SparkSession.builder().master("local[*]").getOrCreate()
 
-    /**   read of csv file **/
+    /** read of csv file * */
     val dataframe: org.apache.spark.sql.DataFrame = sparkSession.read.option("header", true)
-      .csv("hdfs://172.31.250.9:7077/user/namenode/complianceRGPDMS")
+      .csv("data/steph.csv")
 
     val appelShema = sparkSession.createDataFrame(dataframe.rdd, SchemaDonnee.schema)
 
-    /** Show the content of file.csv in our terminal **/
-    val datasfile = dataframe.toDF("IdentificationClient",",Nom","Prenom","Adresse","DateDeSouscription")
-        datasfile.show()
+    /** Show the content of file.csv in our terminal * */
+    val datasfile = dataframe.toDF("IdentificationClient", ",Nom", "Prenom", "Adresse", "DateDeSouscription")
+    datasfile.show()
 
-    /** Hash data from our file and display the result in the terminal **/
-    val generateUUID = udf(() => UUID.randomUUID().toString)
-    val dataHache = datasfile.withColumn("UUID", generateUUID())
-        dataHache.show()
+    import java.util.UUID
+    def MyUuidApp {
+        val uuid = UUID.randomUUID
+        val uuidAsString = uuid.toString
+        System.out.println("Your UUID is: " + uuidAsString)
+      }
+    }
 
-    /** Other chopping methods **/
-     dataframe.toDF("IdentificationClient",",Nom","Prenom","Adresse","DateDeSouscription")
-      .withColumn("uuid", expr("uuid()"))
-      .show(false)
+
+
+      val dfWithUuid = datasfile.withColumn("Nom" )
+      dfWithUuid.show(true)
+
+
+
+
+
+//    def hashage(sparkSession: SparkSession, data: Dataset[schema], id: Long): Dataset[schema] = {
+//      var dset = data
+//      dset =
+//
+//    }
+
+
+
+
+
+    //    /** Hash data from our file and display the result in the terminal **/
+    //    val generateUUID = udf(() => UUID.randomUUID().toString)
+    //    val dataHache = datasfile.withColumn("UUID", generateUUID())
+    //        dataHache.show()
+    //
+    //    /** Other chopping methods **/
+    //     dataframe.toDF("IdentificationClient",",Nom","Prenom","Adresse","DateDeSouscription")
+    //      .withColumn("uuid", expr("uuid()"))
+    //      .show(false)
 
 
   }
